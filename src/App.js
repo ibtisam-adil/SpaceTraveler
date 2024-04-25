779adf8f2ac74aeaabd0179e74e13cd467537f41
ibtisam-adil <ibtisamadil273@gmail.com>
2024-04-25 12:31:18 +0500

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      HELLO
+      <Navbar />
     </div>
   );
 }
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,26 @@
+import './navbar.css';
+// import { NavLink } from 'react-router-dom';
+import logo from '../assets/planet.png';
+
+const Navbar = () => (
+  <nav>
+    <div className="img-h">
+      <img src={logo} alt="logo" className="img" />
+      <h2 className="nav-h">Space Traveller&apos;s Hub</h2>
+    </div>
+    <div className="component-links">
+      <div className="link active">
+        Rockets
+      </div>
+      <div className="link">
+        Missions
+      </div>
+      <div className="links-seperator" />
+      <div className="link">
+        My profile
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
