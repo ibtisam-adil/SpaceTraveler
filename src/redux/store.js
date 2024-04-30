7e4a7dd70d9e55501026c6b24069e0856c996908
ibtisam-adil <ibtisamadil273@gmail.com>
2024-04-30 11:19:30 +0500

    add logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import rocketReducer from './Rocket/rocketSlice';
 
 const store = configureStore({
   reducer: {
     rocketReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
