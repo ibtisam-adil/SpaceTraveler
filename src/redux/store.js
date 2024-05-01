a8ba5c043a3340bc424b64175f9869a21c65e1ee
ibtisam_adil <ibtisamadil273@gmail.com>
2024-05-01 10:31:27 +0500

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,6 @@ const store = configureStore({
     rocketReducer,
     missionReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
